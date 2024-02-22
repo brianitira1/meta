8b32f1b86900b3ff8e105a79b5a8c0f5d94d4b1f
soloKomrade <136370929+brianitira1@users.noreply.github.com>
2024-02-22 05:58:54 +0000

    done with the todo app, will add more features later

--- todoApp.js
+++ todoApp.js
@@ -1,21 +1,76 @@
 class TodoApp {
   constructor(TaskInputField, TaskAddButton) {
     this.PendingTasks = [];
+    this.CompletedTasks = [];
     this.TaskInputField = TaskInputField;
     this.TaskAddButton = TaskAddButton;
+    this.AddTaskmd();
   }
 
   AddTaskmd() {
-    this.TaskAddButton.addEventListener("click", function () {
-      let PendingTaskText = this.TaskInputField.trim();
+    this.TaskAddButton.addEventListener("click", () => {
+      let PendingTaskText = this.TaskInputField.value.trim();
 
-      if (PendingTaskText === "") {
+      if (PendingTaskText !== "") {
         this.PendingTasks.push(PendingTaskText);
         this.TaskInputField.value = "";
+
+        this.UpdateUI();
       } else {
         alert("please input a task ");
       }
     });
+  }
+
+  UpdateUI() {
+    let PendingTaskList = document.getElementById("pendingtasks");
+    PendingTaskList.innerHTML = "";
+
+    this.PendingTasks.forEach((task) => {
+      let TaskItem = document.createElement("li");
+      TaskItem.innerText = task;
+
+      let TaskDeleteButton = document.createElement("button");
+      TaskDeleteButton.textContent = "Delete Task";
+
+      TaskDeleteButton.addEventListener("click", () => {
+        let index = this.PendingTasks.indexOf(task);
+        this.DeletePendingTask(index);
+      });
+
+      let MarkAsDoneButton = document.createElement("button");
+      MarkAsDoneButton.textContent = "Mark As Done";
+
+      MarkAsDoneButton.addEventListener("click", () => {
+        let index = this.PendingTasks.indexOf(task);
+        this.MarkTaskAsDone(index);
+      });
+
+      TaskItem.appendChild(TaskDeleteButton);
+      TaskItem.appendChild(MarkAsDoneButton);
+      PendingTaskList.appendChild(TaskItem);
+    });
+
+    let CompletedTaskList = document.getElementById("completedtasks");
+    CompletedTaskList.innerHTML = "";
+
+    this.CompletedTasks.forEach((task) => {
+      let CompletedTask = document.createElement("li");
+      CompletedTask.innerText = task;
+      CompletedTaskList.appendChild(CompletedTask);
+    });
+  }
+
+  DeletePendingTask(index) {
+    this.PendingTasks.splice(index, 1);
+    this.UpdateUI();
+  }
+
+  MarkTaskAsDone(index) {
+    let taskToMove = this.PendingTasks[index];
+    this.PendingTasks.splice(index, 1);
+    this.CompletedTasks.push(taskToMove);
+    this.UpdateUI();
   }
 }
 
