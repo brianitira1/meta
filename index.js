a36dfd09ee2a8d4cbad4459b371dd27205d38949
soloKomrade <136370929+brianitira1@users.noreply.github.com>
2024-02-23 12:57:55 +0000

    fixing some bugs in the expenses file

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.use(express.static("./"));
 app.use(cors());
 
 app.get("/", function (req, res) {
-  res.sendFile(path.resolve("calc.html"));
+  res.sendFile(path.resolve("expenseTracker.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
